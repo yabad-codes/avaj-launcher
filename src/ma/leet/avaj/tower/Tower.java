f85f6340344bd2666fd1d718eac39ff4ac5b7f4a
Yahya Abad <yabad@c1r5p5.1337.ma>
2024-08-13 22:16:24 +0100

    Logging the right messages for registering and unrestering aircrafts in the tower + documenting code

--- src/ma/leet/avaj/tower/Tower.java
+++ src/ma/leet/avaj/tower/Tower.java
@@ -4,33 +4,50 @@ import java.util.List;
 import java.util.ArrayList;
 import ma.leet.avaj.aircraft.Flyable;
 
+/**
+ * The Tower class represents a weather tower that observes and manages flyable objects.
+ */
 public class Tower {
 	private List<Flyable> observers = new ArrayList<Flyable>();
 
+	/**
+	 * Registers a flyable object to the weather tower.
+	 *
+	 * @param p_flyable the flyable object to register
+	 * @throws IllegalArgumentException if the flyable object is null
+	 */
 	public void register(Flyable p_flyable) {
 		if (p_flyable == null)
 			throw new IllegalArgumentException("Aircraft can't be null");
-		
+
 		if (!observers.contains(p_flyable)) {
 			observers.add(p_flyable);
-			// TODO: LOG A MESSAGE FROM THE TOWERRRRRR AFTER FLYABLE IMPLEMENTATION
+			System.out.println("Tower says: " + p_flyable.toString() + " registered to weather tower.");
 		}
 	}
 
+	/**
+	 * Unregisters a flyable object from the weather tower.
+	 *
+	 * @param p_flyable the flyable object to unregister
+	 * @throws IllegalArgumentException if the flyable object is null
+	 */
 	public void unregister(Flyable p_flyable) {
 		if (p_flyable == null)
 			throw new IllegalArgumentException("Aircraft can't be null");
-		
-		if (observers.remove(p_flyable)) {
-			// TODO: LOG A MESSAGE FROM THE TOWERRRRRR AFTER FLYABLE IMPLEMENTATION
-		}
+
+		if (observers.remove(p_flyable))
+			System.out.println("Tower says: " + p_flyable.toString() + " unregistered from weather tower.");
 	}
 
+	/**
+	 * Notifies all registered flyable objects that the weather condition has changed.
+	 */
 	protected void conditionChanged() {
 		List<Flyable> observsersCopy = new ArrayList<>(observers);
 
 		for (Flyable observer : observsersCopy) {
-			observer.updateConditions(); // NOT IMPLEMENTED YET
+			observer.updateConditions();
 		}
 	}
 }
