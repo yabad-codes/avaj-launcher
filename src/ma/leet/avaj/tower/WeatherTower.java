37db2b6177cbab47c7379f0503cf469609b4ec3a
Yahya Abad <yabad@c1r5p5.1337.ma>
2024-08-13 22:09:02 +0100

    Implementing the WeatherTower class -> concrete implementation of the Tower observable

--- src/ma/leet/avaj/tower/WeatherTower.java
+++ src/ma/leet/avaj/tower/WeatherTower.java
@@ -1,5 +1,27 @@
 package ma.leet.avaj.tower;
 
-public class WeatherTower {
-	
+import ma.leet.avaj.weather.Coordinates;
+import ma.leet.avaj.weather.WeatherProvider;
+
+/**
+ * The WeatherTower class represents a tower that provides weather information.
+ * It extends the Tower class.
+ */
+public class WeatherTower extends Tower {
+	/**
+	 * Gets the current weather at the specified coordinates.
+	 *
+	 * @param coordinates the coordinates to get the weather for
+	 * @return the current weather at the specified coordinates
+	 */
+	public String getWeather(Coordinates coordinates) {
+		return WeatherProvider.getInstance().getCurrentWeather(coordinates);
+	}
+
+	/**
+	 * Changes the weather condition.
+	 */
+	public void changeWeather() {
+		conditionChanged();
+	}
 }
