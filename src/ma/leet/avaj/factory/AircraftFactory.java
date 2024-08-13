d9b739f42897e812c227b1d0fb59d918692fec6e
Yahya Abad <yabad@c1r5p5.1337.ma>
2024-08-13 22:19:43 +0100

    Initialize the singleton pattern for the AircraftFactory

--- src/ma/leet/avaj/factory/AircraftFactory.java
+++ src/ma/leet/avaj/factory/AircraftFactory.java
@@ -1,5 +1,13 @@
 package ma.leet.avaj.factory;
 
 public class AircraftFactory {
-	
+	private static AircraftFactory instance = null;
+
+	private AircraftFactory() {}
+
+	public static AircraftFactory getInstance() {
+		if (instance == null)
+			instance = new AircraftFactory();
+		return instance;
+	}
 }
