ac4ae25faac24e8140f55cbe0896cd6983bc83a3
Yahya Abad <yabad@c1r5p5.1337.ma>
2024-08-13 21:05:21 +0100

    Creating the WeatherProvider class as a singleton pattern

--- src/ma/leet/avaj/weather/WeatherProvider.java
+++ src/ma/leet/avaj/weather/WeatherProvider.java
@@ -1,5 +1,13 @@
 package ma.leet.avaj.weather;
 
 public class WeatherProvider {
-	
+	private static WeatherProvider instance = null;
+
+	private WeatherProvider() {}
+
+	public static WeatherProvider getInstance() {
+		if (instance == null)
+			instance = new WeatherProvider();
+		return instance;
+	}
 }
