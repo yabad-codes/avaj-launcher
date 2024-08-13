9d7cfd1e0a313bca6fe77b870dbc9d68ffceb08a
Yahya Abad <yabad@c1r5p5.1337.ma>
2024-08-13 21:33:28 +0100

    Implementing the Tower observable, still needs work for the logging and optimization

--- src/ma/leet/avaj/tower/Tower.java
+++ src/ma/leet/avaj/tower/Tower.java
@@ -1,5 +1,36 @@
 package ma.leet.avaj.tower;
 
+import java.util.List;
+import java.util.ArrayList;
+import ma.leet.avaj.aircraft.Flyable;
+
 public class Tower {
-	
+	private List<Flyable> observers = new ArrayList<Flyable>();
+
+	public void register(Flyable p_flyable) {
+		if (p_flyable == null)
+			throw new IllegalArgumentException("Aircraft can't be null");
+		
+		if (!observers.contains(p_flyable)) {
+			observers.add(p_flyable);
+			// TODO: LOG A MESSAGE FROM THE TOWERRRRRR AFTER FLYABLE IMPLEMENTATION
+		}
+	}
+
+	public void unregister(Flyable p_flyable) {
+		if (p_flyable == null)
+			throw new IllegalArgumentException("Aircraft can't be null");
+		
+		if (observers.remove(p_flyable)) {
+			// TODO: LOG A MESSAGE FROM THE TOWERRRRRR AFTER FLYABLE IMPLEMENTATION
+		}
+	}
+
+	protected void conditionChanged() {
+		List<Flyable> observsersCopy = new ArrayList<>(observers);
+
+		for (Flyable observer : observsersCopy) {
+			observer.updateConditions(); // NOT IMPLEMENTED YET
+		}
+	}
 }
