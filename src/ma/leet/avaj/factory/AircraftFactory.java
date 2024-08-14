d6d91e13e2e847408007a6ebf97bdcd1d78ffc02
Yahya Abad <yabad@c1r5p5.1337.ma>
2024-08-14 16:24:34 +0100

    aircraft factory doc

--- src/ma/leet/avaj/factory/AircraftFactory.java
+++ src/ma/leet/avaj/factory/AircraftFactory.java
@@ -6,18 +6,32 @@ import ma.leet.avaj.aircraft.Helicopter;
 import ma.leet.avaj.aircraft.JetPlane;
 import ma.leet.avaj.weather.Coordinates;
 
+/**
+ * AircraftFactory
+ */
 public class AircraftFactory {
 	private static AircraftFactory instance = null;
 	private static long nextId = 1;
 
 	private AircraftFactory() {}
 
+	/**
+	 * Get the singleton instance of the AircraftFactory
+	 * @return The singleton instance of the AircraftFactory
+	 */
 	public static AircraftFactory getInstance() {
 		if (instance == null)
 			instance = new AircraftFactory();
 		return instance;
 	}
 
+	/**
+	 * Create a new aircraft
+	 * @param p_type The type of the aircraft
+	 * @param p_name The name of the aircraft
+	 * @param p_coordinates The coordinates of the aircraft
+	 * @return The new aircraft
+	 */
 	public Flyable newAircraft(String p_type, String p_name, Coordinates p_coordinates) {
 		if (p_type == null || p_name == null || p_coordinates == null)
 			throw new IllegalArgumentException("Type, name, and coordinates must not be null");
